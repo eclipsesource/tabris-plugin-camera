26f5a7fc4d8e568b3647d19d61c6852d6d5cb29c
Elshad Seyidmammadov <elshad@eclipsesource.com>
2020-05-27 14:36:10 +0400

    Fix FileProvider conflicts
    The build of a Tabris.js app that contains the 'tabris-plugin-camera' plugin fails due to the manifets merger exception. The exception occurs because of the FileProvider, which specified in the manifets file in both in the plugin and the 'tabris-android' platform. The same exception will be thrown again if another plugin also specifies the same provider.
    
    The change creates a custom FileProvider, which just extends FileProvider and specifies in the manifest to avoid such conflicts.

---
+++ src/android/FileProvider.java
@@ -0,0 +1,8 @@
+package org.apache.cordova.camera;
+
+/** 
+ * The class is only used in the manifest file and 
+ * avoids conflicts with other plugins that use FileProvider.
+ */
+public class FileProvider extends androidx.core.content.FileProvider {
+}
